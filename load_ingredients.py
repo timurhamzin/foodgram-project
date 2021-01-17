96d186a984a05cb8dbd42da98a9cee486d868013
Timur <timur.hamzin@gmail.com>
2021-01-17 15:32:14 +0500

    Fix link in footer

--- load_ingredients.py
+++ load_ingredients.py
@@ -6,5 +6,5 @@ from recipe.models import Ingridient
 if __name__ == '__main__':
     with open('ingredients.json') as f:
         ingredients = json.load(f)
-    [Ingridient(title=i['title'], measurement_unit=i['dimension']).save()
-     for i in ingredients]
+        [Ingridient(title=i['title'], measurement_unit=i['dimension']).save()
+         for i in ingredients]

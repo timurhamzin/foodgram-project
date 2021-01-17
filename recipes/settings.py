762bb8334f578ee219a3a637d16c5e7fa825e244
Timur <timur.hamzin@gmail.com>
2021-01-17 14:28:17 +0500

    Move secret key to .env

--- recipes/settings.py
+++ recipes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0hp8q+2u6sskp)m+xc%(+*_d-d$gxy=zak6o@e^p6zam@ycm8u'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')

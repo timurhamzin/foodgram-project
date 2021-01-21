20e169b481682deedac0d2f84e2cb6c60dbee72b
Timur <timur.hamzin@gmail.com>
2021-01-21 20:05:15 +0500

    Remove unused template tags

--- recipe/templatetags/dict_get.py
+++ recipe/templatetags/dict_get.py
@@ -4,9 +4,3 @@ from django.template.defaultfilters import register
 @register.simple_tag
 def dict_get(a_dict, key, default=''):
     return a_dict.get(key, default)
-
-
-@register.simple_tag
-def join_strings(strings, separator):
-    if strings:
-        return separator.join(strings)
--- recipe/templatetags/in_list.py
+++
@@ -1,6 +0,0 @@
-from django.template.defaultfilters import register
-
-
-@register.simple_tag
-def in_list(val, vals):
-    return val in vals
